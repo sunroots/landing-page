f954d1c37bcddf37287fd6d27e256920a9f6c9bd
Adria Castany <adria.castany@haufe.com>
2020-07-20 17:18:51 +0200

    Restore social links although invalid

--- config.js
+++ config.js
@@ -7,27 +7,27 @@ module.exports = {
   manifestThemeColor: '#663399',
   manifestDisplay: 'standalone',
   manifestIcon: 'src/assets/img/website-icon.png',
-  pathPrefix: `/sunroots/`, // This path is subpath of your hosting https://domain/portfolio
+  // pathPrefix: `/sunroots/`, // This path is subpath of your hosting https://domain/portfolio
   // social
   heading: 'Sunroots',
   subHeading:
     "Del astro rey al solarium de Sandro Rey.",
   socialLinks: [
-    // {
-    //   icon: 'fa-github',
-    //   name: 'Github',
-    //   url: 'https://github.com/anubhavsrivastava',
-    // },
-    // {
-    //   icon: 'fa-twitter',
-    //   name: 'Twitter',
-    //   url: 'https://twitter.com/onlyanubhav',
-    // },
-    // {
-    //   icon: 'fa-facebook',
-    //   name: 'Facebook',
-    //   url: 'https://facebook.com/theanubhav',
-    // },
+    {
+      icon: 'fa-github',
+      name: 'Github',
+      url: 'https://github.com/sunroots/landing-page',
+    },
+    {
+      icon: 'fa-twitter',
+      name: 'Twitter',
+      url: 'https://twitter.com/onlyanubhav',
+    },
+    {
+      icon: 'fa-facebook',
+      name: 'Facebook',
+      url: 'https://facebook.com/theanubhav',
+    },
     {
       icon: 'fa-envelope-o',
       name: 'Email',
