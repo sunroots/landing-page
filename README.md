4b4b29a90a6f4ddc24f22ddf41cd0ccce42634ce
Adria Castany <adria.castany@haufe.com>
2020-07-20 17:02:17 +0200

    Initial commit, sunroots landing page started from Gatsby starter: https://github.com/anubhavsrivastava/gatsby-starter-eventually initial data replaced, demo content removed

---
+++ README.md
@@ -0,0 +1,23 @@
+# Sunroots
+Official landing page
+
+## Install
+
+Make sure that you have the Gatsby CLI program installed:
+
+```sh
+npm install --global gatsby-cli
+```
+
+Then you can run it by:
+
+```sh
+npm install
+gatsby develop
+```
+
+### Deploying using Github page
+
+`package.json` has a default script that uses `gh-pages` module to publish on Github pages. Simply running `npm run deploy` would publish the site on github pages.
+
+Additionally, it also has [path-prefix](https://www.gatsbyjs.org/docs/path-prefix/) value set for gatsby config in `config.js`. Change `pathPrefix` to relevant path if your gatsby site is hosted on subpath of a domain, `https://theanubhav.com/somePath/`. If you are hosting it as root site, i.e, `https://theanubhav.com/` , remove the pathPrefix configuration.
---
+++ config.js
@@ -0,0 +1,37 @@
+module.exports = {
+  siteTitle: 'Sunroots - Del astro rey al solarium de Sandro Rey', // <title>
+  manifestName: 'Sunroots',
+  manifestShortName: 'Sunroots', // max 12 characters
+  manifestStartUrl: '/',
+  manifestBackgroundColor: '#663399',
+  manifestThemeColor: '#663399',
+  manifestDisplay: 'standalone',
+  manifestIcon: 'src/assets/img/website-icon.png',
+  pathPrefix: `/sunroots/`, // This path is subpath of your hosting https://domain/portfolio
+  // social
+  heading: 'Sunroots',
+  subHeading:
+    "Del astro rey al solarium de Sandro Rey.",
+  socialLinks: [
+    // {
+    //   icon: 'fa-github',
+    //   name: 'Github',
+    //   url: 'https://github.com/anubhavsrivastava',
+    // },
+    // {
+    //   icon: 'fa-twitter',
+    //   name: 'Twitter',
+    //   url: 'https://twitter.com/onlyanubhav',
+    // },
+    // {
+    //   icon: 'fa-facebook',
+    //   name: 'Facebook',
+    //   url: 'https://facebook.com/theanubhav',
+    // },
+    {
+      icon: 'fa-envelope-o',
+      name: 'Email',
+      url: 'mailto:castany.adria@gmail.com',
+    },
+  ],
+};
