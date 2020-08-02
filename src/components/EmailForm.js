481182d8890532e4532a5ddb29df524168326f7c
Adria Castany <adria.castany@haufe.com>
2020-08-02 19:55:37 +0200

    Renamed start script, passed to functional component EmailForm

--- src/components/EmailForm.js
+++ src/components/EmailForm.js
@@ -1,39 +1,29 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 
-export class EmailForm extends Component {
-  constructor() {
-    super();
-    this.state = { message: '' };
-    this.onSubmit = this.onSubmit.bind(this);
-  }
+const EmailForm = () => {
+  const [message, setMessage] = useState('');
 
-  onSubmit(e) {
+  const onSubmit = e => {
     e.preventDefault();
     e.stopPropagation();
-    this.setState({ message: 'Thank you!' });
-    setTimeout(() => {
-      this.setState({ message: '' });
-    }, 3000);
+    setMessage('Thank you!');
   }
 
-  render() {
-    const { message } = this.state;
-    return (
-      <form id="signup-form" name="signup-form" onSubmit={this.onSubmit} method="post" action="#" data-netlify="true">
-        <input type="hidden" name="form-name" value="signup-form" />
-        <input
-          type="email"
-          name="email"
-          id="email"
-          placeholder="Tu Email"
-        />
-        <input type="submit" value="Cuéntame más" />
-        <span className={`${message ? 'visible success' : ''} message`}>
-          {message}
-        </span>
-      </form>
-    );
-  }
+  return (
+    <form id="signup-form" name="signup-form" onSubmit={onSubmit} method="post" action="#" data-netlify="true">
+      <input type="hidden" name="form-name" value="signup-form" />
+      <input
+        type="email"
+        name="email"
+        id="email"
+        placeholder="Tu Email"
+      />
+      <input type="submit" value="Cuéntame más" />
+      <span className={`${message ? 'visible success' : ''} message`}>
+        {message}
+      </span>
+    </form>
+  )
 }
 
 export default EmailForm;
